1f7e04a4a15069038ff22feed7b96b0becc2c0b7
Allen Patric A Garcia <55261156+Allen-Patric@users.noreply.github.com>
2020-02-26 19:16:42 -0700

    Create Home.js

---
+++ react-poke/src/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import './App.css';
+import axios from 'axios';
+
+function Home() {
+ 
+  return (
+      <h1>
+          <p>
+            Place holder text
+          </p>
+          <br></br>
+          <p>
+              Features:
+
+          </p>
+          <br></br>
+          <ul>
+          <li>Pokemon List Inventory</li>
+          <li>Favorite your favorite Pokemon</li>
+           </ul>
+      </h1>
+  );
+}
+export default Home
